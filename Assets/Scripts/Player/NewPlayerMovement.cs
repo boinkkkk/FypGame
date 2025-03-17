0a552f4a4e9ba3161f52b503164b041b77f17b4c
boinkkkk <124036574+boinkkkk@users.noreply.github.com>
2025-03-17 22:28:41 +0800

    re-enable rpc

--- Assets/Scripts/Player/NewPlayerMovement.cs
+++ Assets/Scripts/Player/NewPlayerMovement.cs
@@ -42,123 +42,10 @@ public class NewPlayerMovement : NetworkBehaviour
         // if (!IsOwner) return; // Only the owner can control movement
 
 // uncomment from here
-    //     Move = Input.GetAxis("Horizontal");
-    //     // Send movement input to the server
-    //     MoveServerRpc(Move);
-
-    //     rb.velocity = new Vector2( Move * speed, rb.velocity.y);
-    //     if(Input.GetKeyDown(KeyCode.LeftShift)) {
-    //         animator.Play("WalkingAnim");
-    //     }
-
-    //     // Flip the sprite based on movement direction
-    //     if (Move > 0) // Moving right
-    //     {
-    //         spriteRenderer.flipX = false;
-    //         animator.SetBool("isWalking", true);
-    //         // animator.Play("WalkingState");
-    //     }
-    //     else if (Move < 0) // Moving left
-    //     {
-    //         spriteRenderer.flipX = true;
-    //         animator.SetBool("isWalking", true);
-    //     }
-
-    //     else {
-    //         animator.SetBool("isWalking", false);
-    //     }
-
-        
-    //     // if (Input.GetButtonDown("Jump") && isJumping == false) 
-    //     // {
-    //     //     rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-    //     // }
-
-    //     // Handle Jumping
-    //     if (Input.GetButtonDown("Jump") && !isJumping)
-    //     {
-    //         JumpServerRpc();
-    //     }
-
-    // }
-
-    // [ServerRpc]
-    // void MoveServerRpc(float move)
-    // {
-    //     rb.velocity = new Vector2(move * speed, rb.velocity.y);
-
-    //     // Sync animation and sprite flipping across clients
-    //     FlipClientRpc(move);
-    // }
-
-    // [ServerRpc]
-    // void JumpServerRpc()
-    // {
-    //     if (!isJumping)
-    //     {
-    //         rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-    //         isJumping = true;
-    //     }
-    // }
-
-    // [ClientRpc]
-    // void FlipClientRpc(float move)
-    // {
-    //     if(!IsOwner) return; //Prevent running on non-owners
-    //     if (spriteRenderer == null || animator == null) return; // Prevent null error
-
-    //     if (move > 0)
-    //     {
-    //         spriteRenderer.flipX = false;
-    //         animator.SetBool("isWalking", true);
-    //     }
-    //     else if (move < 0)
-    //     {
-    //         spriteRenderer.flipX = true;
-    //         animator.SetBool("isWalking", true);
-    //     }
-    //     else
-    //     {
-    //         animator.SetBool("isWalking", false);
-    //     }
-    // }
-
-    // //  void FixedUpdate()
-    // // {
-    // //     // Ground check using an overlap circle
-    // //     isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
-
-    // //     if (Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer))
-    // //     {
-    // //         isGrounded = true;
-    // //         groundGraceCounter = groundGraceTime; // Reset the grace counter
-    // //     }
-    // //     else
-    // //     {
-    // //         groundGraceCounter -= Time.deltaTime;
-    // //         if (groundGraceCounter <= 0)
-    // //         {
-    // //             isGrounded = false;
-    // //         }
-    // //     }
-    // // }
-
-    // private void OnCollisionEnter2D(Collision2D other) {
-    //     // if player is on the ground, means not jumping
-    //     if (other.gameObject.CompareTag("Ground")) 
-    //     {
-    //         isJumping = false;
-    //     }
-    // }
-    // private void OnCollisionExit2D(Collision2D other) {
-    //     // if player is NOT on the ground, means jumping
-    //     if (other.gameObject.CompareTag("Ground")) 
-    //     {
-    //         isJumping = true;
-    //     }
-    // }
-
         Move = Input.GetAxis("Horizontal");
+        // Send movement input to the server
+        MoveServerRpc(Move);
+
         rb.velocity = new Vector2( Move * speed, rb.velocity.y);
         if(Input.GetKeyDown(KeyCode.LeftShift)) {
             animator.Play("WalkingAnim");
@@ -180,13 +67,81 @@ public class NewPlayerMovement : NetworkBehaviour
         else {
             animator.SetBool("isWalking", false);
         }
+
         
-        if (Input.GetButtonDown("Jump") && isJumping == false) 
+        // if (Input.GetButtonDown("Jump") && isJumping == false) 
+        // {
+        //     rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        // }
+
+        // Handle Jumping
+        if (Input.GetButtonDown("Jump") && !isJumping)
         {
-            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            JumpServerRpc();
         }
 
     }
+
+    [ServerRpc]
+    void MoveServerRpc(float move)
+    {
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        // Sync animation and sprite flipping across clients
+        FlipClientRpc(move);
+    }
+
+    [ServerRpc]
+    void JumpServerRpc()
+    {
+        if (!isJumping)
+        {
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    [ClientRpc]
+    void FlipClientRpc(float move)
+    {
+        if(!IsOwner) return; //Prevent running on non-owners
+        if (spriteRenderer == null || animator == null) return; // Prevent null error
+
+        if (move > 0)
+        {
+            spriteRenderer.flipX = false;
+            animator.SetBool("isWalking", true);
+        }
+        else if (move < 0)
+        {
+            spriteRenderer.flipX = true;
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    //  void FixedUpdate()
+    // {
+    //     // Ground check using an overlap circle
+    //     isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+    //     if (Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer))
+    //     {
+    //         isGrounded = true;
+    //         groundGraceCounter = groundGraceTime; // Reset the grace counter
+    //     }
+    //     else
+    //     {
+    //         groundGraceCounter -= Time.deltaTime;
+    //         if (groundGraceCounter <= 0)
+    //         {
+    //             isGrounded = false;
+    //         }
+    //     }
+    // }
 
     private void OnCollisionEnter2D(Collision2D other) {
         // if player is on the ground, means not jumping
@@ -202,4 +157,51 @@ public class NewPlayerMovement : NetworkBehaviour
             isJumping = true;
         }
     }
+
+
+// Uncomment here
+    //     Move = Input.GetAxis("Horizontal");
+    //     rb.velocity = new Vector2( Move * speed, rb.velocity.y);
+    //     if(Input.GetKeyDown(KeyCode.LeftShift)) {
+    //         animator.Play("WalkingAnim");
+    //     }
+
+    //     // Flip the sprite based on movement direction
+    //     if (Move > 0) // Moving right
+    //     {
+    //         spriteRenderer.flipX = false;
+    //         animator.SetBool("isWalking", true);
+    //         // animator.Play("WalkingState");
+    //     }
+    //     else if (Move < 0) // Moving left
+    //     {
+    //         spriteRenderer.flipX = true;
+    //         animator.SetBool("isWalking", true);
+    //     }
+
+    //     else {
+    //         animator.SetBool("isWalking", false);
+    //     }
+        
+    //     if (Input.GetButtonDown("Jump") && isJumping == false) 
+    //     {
+    //         rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+    //     }
+
+    // }
+
+    // private void OnCollisionEnter2D(Collision2D other) {
+    //     // if player is on the ground, means not jumping
+    //     if (other.gameObject.CompareTag("Ground")) 
+    //     {
+    //         isJumping = false;
+    //     }
+    // }
+    // private void OnCollisionExit2D(Collision2D other) {
+    //     // if player is NOT on the ground, means jumping
+    //     if (other.gameObject.CompareTag("Ground")) 
+    //     {
+    //         isJumping = true;
+    //     }
+    // }
 }
