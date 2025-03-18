ef87cbf15a9028132618218aecf66d9b0e344eb0
boinkkkk <124036574+boinkkkk@users.noreply.github.com>
2025-03-19 02:40:45 +0800

    update playerdeath
    cant reload scene but can respawn players without reloading scene

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -1,82 +1,12 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-
-// public class PlayerDeath : MonoBehaviour
-// {
-//     public Rigidbody2D rb;
-//     public GameObject respawnPoint; // Optional: For respawning the player
-//     public float deathDelay = 1.5f; // Delay before death effect
-//     Animator animator;
-
-
-//  void Start()
-//     {
-//         rb = GetComponent<Rigidbody2D>(); // Get the RigidBody
-//         animator = GetComponent<Animator>();
-//     }
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         // Check if the player collides with the spikes
-//         if (other.CompareTag("Enemy"))
-//         {
-//             Die();
-//         }
-//     }
-
-//     private void Die()
-//     {
-//         // Example: Display a death effect, disable controls, or reload the scene
-//         Debug.Log("Player Died!");
-
-//         animator.SetTrigger("Die");
-//         Invoke("RestartScene", deathDelay);
-
-//         // Disable movement or player controls (optional)
-//         GetComponent<NewPlayerMovement>().enabled = false;
-        
-//         // Optional: Restart the scene (uncomment to use)
-//         // SceneManager.LoadScene(SceneManager.GetActiveScene().LevelSample);
-//         // SceneManager.LoadSceneAsync("LevelSample");
-        
-//         // Optional: Respawn the player (if you have a respawn point)
-//         // animator.SetTrigger("Die");
-//         // StartCoroutine(Respawn());
-         
-//     }
-//     private void RestartScene()
-//     {
-//         // Reload the current scene
-//         SceneManager.LoadSceneAsync("LevelSample");
-//         // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-//         StartCoroutine(Respawn());
-//     }
-
-//     // Optional respawn coroutine
-//     private IEnumerator Respawn()
-//     {
-//         animator.SetTrigger("Die");
-//         // Disable the player temporarily
-//         // gameObject.SetActive(false);
-
-//         // Wait for the death delay
-//         yield return new WaitForSeconds(deathDelay);
-
-//         // Move to the respawn point and re-enable the player
-//         transform.position = respawnPoint.transform.position;
-//         gameObject.SetActive(true);
-//     }
-// }
-
 using System.Collections;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.SceneManagement;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
 using System.Threading.Tasks;
 
-public class PlayerDeath : MonoBehaviour
+public class PlayerDeath : NetworkBehaviour
 {
     public Rigidbody2D rb;
     public GameObject respawnPoint; // Set this in Unity
@@ -111,6 +41,12 @@ public class PlayerDeath : MonoBehaviour
         rb.gravityScale = 0;
 
         StartCoroutine(RespawnCoroutine());
+
+        // If the server detects death, restart the scene for everyone
+        // if (IsServer)
+        // {
+        //     StartCoroutine(ResetSceneForAllPlayers());
+        // }
     }
 
     private IEnumerator RespawnCoroutine()
@@ -131,5 +67,17 @@ public class PlayerDeath : MonoBehaviour
         rb.gravityScale = 7;  // Restore gravity
 
         isRespawning = false;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ResetSceneForAllPlayersServerRpc()
+    {
+        NetworkManager.Singleton.SceneManager.LoadScene("LevelSample", LoadSceneMode.Single);
+    }
+
+    private IEnumerator ResetSceneForAllPlayers()
+    {
+        yield return new WaitForSeconds(deathDelay);
+        ResetSceneForAllPlayersServerRpc();
     }
 }
