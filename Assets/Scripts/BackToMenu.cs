bb3d1f5bbd4d41b2af6ae753072616a91089b1d8
boinkkkk <124036574+boinkkkk@users.noreply.github.com>
2025-04-09 01:34:13 +0800

    sign out before returning to main menu
    to disconnect from unity service

--- Assets/Scripts/BackToMenu.cs
+++ Assets/Scripts/BackToMenu.cs
@@ -1,17 +1,43 @@
   using UnityEngine;
   using UnityEngine.SceneManagement;
+  using Unity.Services.Authentication;
+  using System.Collections;
 
   public class BackToMenu : MonoBehaviour
   {
       // Method to load a scene by name
-      public void LoadSceneByName(string sceneName)
-      {
-          SceneManager.LoadScene(sceneName);
-      }
+    //   public void LoadSceneByName(string sceneName)
+    //   {
+    //       SceneManager.LoadScene(sceneName);
+    //   }
 
-      // Method to load a scene by build index
-      public void LoadSceneByIndex(int sceneIndex)
-      {
-          SceneManager.LoadScene(sceneIndex);
-      }
-  }
+    //   // Method to load a scene by build index
+    //   public void LoadSceneByIndex(int sceneIndex)
+    //   {
+    //       SceneManager.LoadScene(sceneIndex);
+    //   }
+
+
+
+
+    public void SignOutAndChangeScene(string sceneName)
+    {
+        // Sign out the user
+        AuthenticationService.Instance.SignOut(true);
+        Debug.Log("User signed out successfully.");
+
+        // Load the specified scene
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncLoad.isDone)
+        {
+            yield return null; // Wait until the scene is fully loaded
+        }
+        Debug.Log($"Scene '{sceneName}' loaded successfully.");
+    }
+}
+
